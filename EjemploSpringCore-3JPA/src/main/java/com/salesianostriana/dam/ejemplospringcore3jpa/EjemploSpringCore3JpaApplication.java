a7867aa7b3e6eaefac807295c750650b052389fa
Daniel Martínez León <daniel1martinezleon@gmail.com>
2024-04-23 12:44:31 +0200

    a

---
+++ EjemploSpringCore-3JPA/src/main/java/com/salesianostriana/dam/ejemplospringcore3jpa/EjemploSpringCore3JpaApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.ejemplospringcore3jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploSpringCore3JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploSpringCore3JpaApplication.class, args);
+	}
+
+}
---
+++ EjemploSpringCore-3JPA/src/test/java/com/salesianostriana/dam/ejemplospringcore3jpa/EjemploSpringCore3JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.ejemplospringcore3jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploSpringCore3JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
