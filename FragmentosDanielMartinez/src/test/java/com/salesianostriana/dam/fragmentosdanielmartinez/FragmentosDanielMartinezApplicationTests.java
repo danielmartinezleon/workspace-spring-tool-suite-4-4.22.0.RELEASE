aff83b3cd72f5ccc87d6fd86cf5a53594418a1de
Daniel Martínez León <daniel1martinezleon@gmail.com>
2024-04-25 13:35:38 +0200

    Ejemplo Prog Fragmentos

---
+++ FragmentosDanielMartinez/src/test/java/com/salesianostriana/dam/fragmentosdanielmartinez/FragmentosDanielMartinezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.fragmentosdanielmartinez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FragmentosDanielMartinezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
