99d0ed72dcdfe02e4dff0a7fd4cbb9712dc62e9c
Daniel Martínez León <daniel1martinezleon@gmail.com>
2024-05-02 14:25:06 +0200

    Ejemplos sobre herencia

---
+++ herencia/src/main/java/com/salesianostriana/dam/herencia/model/Bebida.java
@@ -0,0 +1,22 @@
+package com.salesianostriana.dam.herencia.model;
+
+import jakarta.persistence.Entity;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+@Entity
+public class Bebida extends Producto{
+
+	private int tamanio;
+
+	public Bebida(String nombre, float precio, int tamanio) {
+		super(nombre, precio);
+		this.tamanio = tamanio;
+	}	
+}
---
+++ herencia/src/main/java/com/salesianostriana/dam/herencia/model/Producto.java
@@ -0,0 +1,25 @@
+package com.salesianostriana.dam.herencia.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@MappedSuperclass
+public abstract class Producto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	private String nombre;
+	
+	private float precio;
+	
+	public Producto(String nombre, float precio) {
+		this.nombre = nombre;
+		this.precio = precio;
+	}
+}
